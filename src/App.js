d4a9f2a286f6905506b87b8a0f33af5c7298827a
Emilie G <adamanska@gmail.com>
2018-02-15 14:47:59 +0100

    REAAAADY

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
 import Titles from "./components/Titles";
-import Form from "./components/Form";
 import Weather from "./components/Weather";
+import axios from 'axios';
+
 
 const API_KEY = "7d19523678b80c45bc75b140ae11de1d";
 
 class App extends Component {
   // eslint-disable-next-line
-  getWeather = async (e) => {
-    e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-    console.log(data);
+
+
+  constructor() {
+    super();
+    this.state = {
+      weatherdata: [],
+    }
   }
+
+  componentDidMount() {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Barcelona,Spain&appid=7d19523678b80c45bc75b140ae11de1d&units=metric`)
+      .then(res => {
+        console.log(res)
+        this.setState({ weatherdata: res.data });
+      })
+  }
+
+
+
   render() {
+    console.log(this.state.weatherdata.weather)
     return (
       <div>
         Start
         <Titles />
-        <Form getWeather={this.getWeather}/>
         <Weather />
+
       </div>
     );
   }
 }
 
 export default App;
+
+// if (this.state.weatherdata.length > 0)
