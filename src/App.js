98c6ba55c72d0a46f073740d3cf97449ff9d09b5
Emilie G <adamanska@gmail.com>
2018-02-15 18:02:38 +0100

    Calls done

--- src/App.js
+++ src/App.js
@@ -4,41 +4,45 @@ import Weather from "./components/Weather";
 import axios from 'axios';
 
 
-const API_KEY = "7d19523678b80c45bc75b140ae11de1d";
+// const API_KEY = "7d19523678b80c45bc75b140ae11de1d";
 
 class App extends Component {
   // eslint-disable-next-line
-
-
   constructor() {
     super();
     this.state = {
       weatherdata: [],
-      weatherdesc: "",
-      weathertemp: "",
+      weathermain: "",
+      weatherdes: "",
+      weathertempmax: "",
+      weathertempmin: "",
+      weathercloud: "",
+      weatherrain: "",
+      weathername: "",
     }
   }
 
   componentDidMount() {
     axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Barcelona,Spain&appid=7d19523678b80c45bc75b140ae11de1d&units=metric`)
       .then(res => {
-        console.log(res)
-        console.log(res.data.weather[0].main)
+        // console.log(res)
         this.setState({ 
           weatherdata: res.data,
-          weatherdesc: res.data.weather[0].main,
-          weathertemp: res.data.main.temp, 
+          weathermain: res.data.weather[0].main,
+          weatherdes: res.data.weather[0].description,
+          weathertempmax: res.data.main.temp_max, 
+          weathertempmin: res.data.main.temp_min,
+          weathercloud: res.data.clouds.all,
+          weatherrain: res.data.rain3h,
+          weathername: res.data.name,  
         });
       })
   }
 
-
-
   render() {
-    console.log(this.state.weatherdesc)
+    console.log(this.state)
     return (
       <div>
-        Start
         <Titles />
         <span>{this.state.weathertemp}</span>
         <span>{this.state.weatherdesc}</span>
